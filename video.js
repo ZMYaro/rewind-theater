a6045d385e650b1911b41eb020d08ef3d0583c28
Zachary Yaro <zmyaro@gmail.com>
2013-11-13 15:38:21 -0500

    Moved keyboard shortcuts code to its own JS file

--- video.js
+++ video.js
@@ -81,31 +81,3 @@ window.addEventListener('load', function() {
 	document.getElementById('fwdBtn').addEventListener('mouseup', stopFwd, false);
 	document.getElementById('vidUpload').addEventListener('change', loadVideo, false);
 }, false);
-
-window.addEventListener('keydown', function(e) {
-	switch(e.keyCode) {
-		case 32: // space
-			playPause(e);
-			break;
-		case 37: // left
-			startRew(e);
-			document.getElementById('rewBtn').disabled = true;
-			break;
-		case 39: // right
-			startFwd(e);
-			document.getElementById('fwdBtn').disabled = true;
-			break;
-	}
-}, false);
-window.addEventListener('keyup', function(e) {
-	switch(e.keyCode) {
-		case 37: // left
-			stopRew(e);
-			document.getElementById('rewBtn').disabled = false;
-			break;
-		case 39: // right
-			stopFwd(e);
-			document.getElementById('fwdBtn').disabled = false;
-			break;
-	}
-}, false);

48d292c990afd00e407ad9417aab7ed222bd761a
Zachary Yaro <zmyaro@gmail.com>
2013-11-13 15:41:32 -0500

    Add keyboard shortcut to clear canvas and preventDefault in key listeners

--- keyboard.js
+++ keyboard.js
@@ -1,26 +1,31 @@
 window.addEventListener('keydown', function(e) {
+	e.preventDefault();
 	switch(e.keyCode) {
+		case 27: // esc
+			clearCanvas();
+			break;
 		case 32: // space
-			playPause(e);
+			playPause();
 			break;
 		case 37: // left
-			startRew(e);
+			startRew();
 			document.getElementById('rewBtn').disabled = true;
 			break;
 		case 39: // right
-			startFwd(e);
+			startFwd();
 			document.getElementById('fwdBtn').disabled = true;
 			break;
 	}
 }, false);
 window.addEventListener('keyup', function(e) {
+	e.preventDefault();
 	switch(e.keyCode) {
 		case 37: // left
-			stopRew(e);
+			stopRew();
 			document.getElementById('rewBtn').disabled = false;
 			break;
 		case 39: // right
-			stopFwd(e);
+			stopFwd();
 			document.getElementById('fwdBtn').disabled = false;
 			break;
 	}

1dd204f15e456ee765420d7a19ca65e852f59ca8
Zachary Yaro <zmyaro@zmyaro.com>
2021-03-05 04:18:32 -0500

    Consolidate duplicate keyboard shortcut functions

--- keyboard.js
+++ keyboard.js
@@ -4,62 +4,39 @@ window.addEventListener('keydown', function (e) {
 		return;
 	}
 	switch (e.keyCode) {
-		case 27: // Esc = clear annotations
-			e.preventDefault();
-			clearCanvas();
-			break;
-		case 32: // Space = play/pause
-			e.preventDefault();
-			playPause();
-			break;
-		case 33: // PgUp = speed up
-			e.preventDefault();
-			increaseSpeed();
-			break;
-		case 34: // PgDn = speed down
-			e.preventDefault();
-			decreaseSpeed();
-			break;
-		case 37: // Left = rewind
-			e.preventDefault();
-			startRew();
-			document.getElementById('rew-btn').disabled = true;
-			break;
-		case 38: // Up = speed up
-			e.preventDefault();
-			increaseSpeed();
-			break;
-		case 39: // Right = fast forward
-			e.preventDefault();
-			startFwd();
-			document.getElementById('fwd-btn').disabled = true;
-			break;
-		case 40: // Down = speed down
-			e.preventDefault();
-			decreaseSpeed();
-			break;
+		case 27: // Esc =
 		case 67: // C = clear annotations
 			e.preventDefault();
 			clearCanvas();
 			break;
-		case 69: // E = ellipse tool
+		case 32: // Space =
+		case 75: // K = play/pause
 			e.preventDefault();
-			setEllipseTool();
+			playPause();
 			break;
+		case 33: // PgUp =
+		case 38: // Up = speed up
+			e.preventDefault();
+			increaseSpeed();
+			break;
+		case 34: // PgDn =
+		case 40: // Down = speed down
+			e.preventDefault();
+			decreaseSpeed();
+			break;
+		case 37: // Left =
 		case 74: // J = rewind
 			e.preventDefault();
 			startRew();
 			document.getElementById('rew-btn').disabled = true;
 			break;
-		case 75: // K = play/pause
-			e.preventDefault();
-			playPause();
-			break;
+		case 39: // Right =
 		case 76: // L = fast forward
 			e.preventDefault();
 			startFwd();
 			document.getElementById('fwd-btn').disabled = true;
 			break;
+		case 69: // E =
 		case 79: // O = ellipse (oval) tool
 			e.preventDefault();
 			setEllipseTool();
@@ -81,20 +58,12 @@ window.addEventListener('keydown', function (e) {
 window.addEventListener('keyup', function (e) {
 	switch (e.keyCode) {
 		case 37: // Left
-			e.preventDefault();
-			stopRew();
-			document.getElementById('rew-btn').disabled = false;
-			break;
-		case 39: // Right
-			e.preventDefault();
-			stopFwd();
-			document.getElementById('fwd-btn').disabled = false;
-			break;
 		case 74: // J
 			e.preventDefault();
 			stopRew();
 			document.getElementById('rew-btn').disabled = false;
 			break;
+		case 39: // Right
 		case 76: // L
 			e.preventDefault();
 			stopFwd();
